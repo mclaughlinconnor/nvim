70f76e98a4f6d9a1b7ff6b9b9f409a9ffbe127ff
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-09-14 20:36:14 +0100

    Lock all plugins to specific commits

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,44 +40,38 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use { "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" } -- Have packer manage itself
-  use { "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" } -- Useful lua functions used by lots of plugins
-  use { "nvim-treesitter/nvim-treesitter", commit = "518e27589c0463af15463c9d675c65e464efc2fe" }
-  use 'shaunsingh/nord.nvim'
-  use 'nvim-treesitter/playground'
-  use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/lsp-status.nvim'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use { "williamboman/mason.nvim" }
-  use { "williamboman/mason-lspconfig.nvim" }
-  use 'nvim-telescope/telescope.nvim'
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-  use 'ludovicchabant/vim-gutentags'
-  use { 'numToStr/Comment.nvim' }
-  use {
-  'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'arkav/lualine-lsp-progress'
-  use 'mbbill/undotree'
-  use "windwp/nvim-autopairs"
-  use "p00f/nvim-ts-rainbow"
-  use "machakann/vim-sandwich"
-  use "norcalli/nvim-colorizer.lua"
-  use "lewis6991/gitsigns.nvim"
-  use "kdheepak/lazygit.nvim"
-  use "AndrewRadev/switch.vim"
-
-  use "saadparwaiz1/cmp_luasnip"
-  -- jsregexp needed for lsp snippet transforms
-  use {"L3MON4D3/LuaSnip", run = "make install_jsregexp"}
-  -- use my fork to include code highlighting for `code` environments
-  use 'mclaughlinconnor/vimtex'
+  use { "numToStr/Comment.nvim", commit = "30d23aa" }
+  use { "arkav/lualine-lsp-progress", commit = "56842d0" }
+  use { "mclaughlinconnor/vimtex", commit = "cff605f4" }
+  use { "mbbill/undotree", commit = "bf76bf2" }
+  use { "saadparwaiz1/cmp_luasnip", commit = "a9de941" }
+  use { "williamboman/mason.nvim", commit = "b56ea0b" }
+  use { "norcalli/nvim-colorizer.lua", commit = "36c610a" }
+  use { "nvim-telescope/telescope-fzf-native.nvim", commit = "65c0ee3", run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use { "L3MON4D3/LuaSnip", run = "make install_jsregexp", commit = "6e506ce" }
+  use { "wbthomason/packer.nvim", commit = "00ec5ad" }
+  use { "windwp/nvim-autopairs", commit = "5fe2441" }
+  use { "ludovicchabant/vim-gutentags", commit = "b77b8fa" }
+  use { "neovim/nvim-lspconfig", commit = "f8b3c24" }
+  use { "kdheepak/lazygit.nvim", commit = "9c73fd6" }
+  use { "AndrewRadev/switch.vim", commit = "900c5d3" }
+  use { "nvim-lua/plenary.nvim", commit = "968a4b9" }
+  use { "lewis6991/gitsigns.nvim", commit = "d7e0bcb" }
+  use { "shaunsingh/nord.nvim", commit = "209e9b3" }
+  use { "kyazdani42/nvim-web-devicons", commit = "2d02a56" }
+  use { "hrsh7th/cmp-nvim-lsp", commit = "affe808" }
+  use { "hrsh7th/nvim-cmp", commit = "913eb85" }
+  use { "nvim-treesitter/playground", commit = "bcfab84" }
+  use { "hrsh7th/cmp-cmdline", commit = "9c0e331" }
+  use { "machakann/vim-sandwich", commit = "74898e6" }
+  use { "nvim-treesitter/nvim-treesitter", commit = "518e2758" }
+  use { "nvim-telescope/telescope.nvim", commit = "2584ff3" }
+  use { "nvim-lua/lsp-status.nvim", commit = "54f48eb" }
+  use { "williamboman/mason-lspconfig.nvim", commit = "1534b61" }
+  use { "nvim-lualine/lualine.nvim", commit = "a52f078", requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+  use { "p00f/nvim-ts-rainbow", commit = "620a24f" }
+  use { "hrsh7th/cmp-path", commit = "447c87c" }
+  use { "hrsh7th/cmp-buffer", commit = "3022dbc" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
