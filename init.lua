fd67dcab4ef32cd06e2ac037bf8a179501d26c55
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-09-17 22:45:54 +0100

    Reformat init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-require "user.options"
-require "user.plugins"
-require "user.colourscheme"
-require "user.treesitter"
-require "user.completion"
-require "user.lsp"
-require "user.telescope"
-require "user.tags"
-require "user.comments"
-require "user.lualine"
-require "user.undotree"
-require "user.brackets"
-require "user.git"
-require "user.snippets"
-require "user.tree"
-require "user.utils.trevj"
+require("user.options")
+require("user.plugins")
+require("user.colourscheme")
+require("user.treesitter")
+require("user.completion")
+require("user.lsp")
+require("user.telescope")
+require("user.tags")
+require("user.comments")
+require("user.lualine")
+require("user.undotree")
+require("user.brackets")
+require("user.git")
+require("user.snippets")
+require("user.tree")
+require("user.utils.trevj")
 
 vim.cmd("source ~/.config/nvim/vim/tex.vim")
