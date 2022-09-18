1ef251fd107a9e0431f5f5ef8bdc37007eb24ba7
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-09-18 16:23:04 +0100

    Add keymap lua snippet

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -1,3 +1,15 @@
 return {
   s("use", fmta([[use({ "<>", commit = "<>" })]], { i(1), i(2) })),
+  s(
+    "key",
+    fmta(
+      [[vim.keymap.set('<>', '<>', <>, { noremap = true, silent = true })]],
+      { c(1, {
+        t("n"),
+        t("i"),
+        t("v"),
+        i(nil),
+      }), i(2), i(3) }
+    )
+  ),
 }, {}
