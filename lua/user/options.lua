e68ed48e422fcbae9f247602dc6844ae11f80277
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-11-16 13:01:53 +0000

    Add termguicolors to use proper colour

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -27,6 +27,7 @@ local options = {
   lazyredraw = true, -- Don't redraw when executing macros
   magic = true, -- Make vim regex more regex-y
   autoread = true, -- re-read files if they are changed
+  termguicolors = true, -- use full colour
 }
 
 vim.opt.shortmess:append("c") -- don't need extra verbose autocomplete
