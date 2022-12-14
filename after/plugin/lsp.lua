069ac942b494a3a2dfbff49c41468e582bf2c1e7
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-12-14 13:23:56 +0000

    Disable stupid auto formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -49,23 +49,10 @@ require("lspconfig").angularls.setup({
   cmd = { "ngserverPUG", "--stdio", "--tsProbeLocations", "", "--ngProbeLocations", "" }
 })
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
   debug = true,
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = on_attach,
-  -- on_attach = function(client, bufnr)
-  --   if client.supports_method("textDocument/formatting") then
-  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  --     vim.api.nvim_create_autocmd("BufWritePre", {
-  --       group = augroup,
-  --       buffer = bufnr,
-  --       callback = function()
-  --         vim.lsp.buf.format({ bufnr = bufnr })
-  --       end,
-  --     })
-  --   end
-  -- end,
   debug = true,
   sources = {
     require("null-ls").builtins.completion.spell,
