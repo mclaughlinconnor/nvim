6ebdad5b8efd7242249bf76ccdd9619ce66fdf11
Connor McLaughlin <connor1211.mcl@gmail.com>
2022-12-24 00:42:47 +0000

    Completely redo completion

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -1,33 +1,48 @@
 -- Setup nvim-cmp.
 local cmp = require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
 cmp.setup({
   preselect = cmp.PreselectMode.None,
   completion = {
     keyword_length = 0,
   },
-  sorting = {
-    priority_weight = 1.0,
-    comparators = {
-      cmp.config.compare.locality,
-      cmp.config.compare.recently_used,
-      -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
-      cmp.config.compare.score,
-      cmp.config.compare.offset,
-      cmp.config.compare.order,
-    },
-  },
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    fields = {
+      cmp.ItemField.Abbr,
+      cmp.ItemField.Kind,
+      cmp.ItemField.Menu,
+    },
+    format = function(entry, vim_item)
+      -- Source
+      vim_item.menu = ({
+        buffer = "[B]",
+        calc = "[C]",
+        cmdline_history = "[CH]",
+        cmp_git = "[CG]",
+        luasnip = "[LS]",
+        luasnip_choice = "[LSC]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[L]",
+        path = "[P]",
+        treesitter = "[TS]",
+        spell = "[S]",
+      })[entry.source.name]
+      return vim_item
+    end,
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<M-;>"] = cmp.mapping.abort(),
-    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<C-j>"] = cmp.mapping.confirm({ select = true }),
     ["<M-j>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -44,10 +59,22 @@ cmp.setup({
     end,
   }),
   sources = cmp.config.sources({
-    { name = "nvim_lsp", priority = 10 },
-    { name = "luasnip", priority = 9 },
+    { name = "luasnip" },
+    { name = "luasnip_choice" },
+    { name = "calc" },
+    { name = "path" },
+    { name = "nvim_lsp" },
   }, {
-    { name = "buffer", priority = 0 },
+    { name = "treesitter" },
+    {
+      name = "spell",
+      option = {
+        enable_in_context = function()
+          return require("cmp.config.context").in_treesitter_capture("spell")
+        end,
+      },
+    },
+    { name = "buffer" },
   }),
 })
 
@@ -56,9 +83,8 @@ cmp.setup.filetype("gitcommit", {
   sources = cmp.config.sources({
     { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
   }, {
-    { name = "buffer" },
-  }, {
     { name = "path" },
+    { name = "buffer" },
   }),
 })
 
@@ -81,6 +107,7 @@ cmp.setup.cmdline("/", {
     end,
   }),
   sources = {
+    { name = "cmdline_history" },
     { name = "buffer" },
   },
 })
@@ -88,24 +115,24 @@ cmp.setup.cmdline("/", {
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline({
-    ["<M-j>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<M-k>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
+    ["<M-j>"] = {
+      c = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+    },
+    ["<M-k>"] = {
+      c = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
+    },
   }),
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
+  sources = cmp.config.sources({ { name = "cmdline" } }, { { name = "path" } }),
 })
