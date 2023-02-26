62aebb926bc5e01db4ca7ffeddf075d3bab7dcc4
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-02-26 21:46:29 +0000

    Add gutentags status line text, reformat

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,10 +1,10 @@
-require('lualine').setup({
+require("lualine").setup({
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics', 'quickfix'},
-    lualine_c = {'filename'},
-    lualine_x = {'lsp_progress'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics", "quickfix" },
+    lualine_c = { "filename", "call gutentags#statusline()" },
+    lualine_x = { "lsp_progress" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
   },
 })
