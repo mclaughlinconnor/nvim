24c8ea5205a305e7f12d950bcd6858bcc93946f4
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-03-01 23:02:43 +0000

    Add - to iskeyword

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,6 +33,7 @@ local options = {
 
 vim.opt.shortmess:append("c") -- don't need extra verbose autocomplete
 vim.cmd("set whichwrap+=<,>,[,],h,l") -- movements can wrap over lines
+vim.cmd("set iskeyword+=-") -- make - part of word
 vim.g.mapleader = ","
 
 for k, v in pairs(options) do
