28a14fad1714fa3cdf4fec9a7d1948360f86b123
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-06-18 20:15:39 +0100

    Add semicolon to console log snippet

--- luasnippets/typescript.lua
+++ luasnippets/typescript.lua
@@ -1,7 +1,7 @@
 return {
   postfix(".log", {
     f(function(_, parent)
-      return "console.log(" .. parent.snippet.env.POSTFIX_MATCH .. ")"
+      return "console.log(" .. parent.snippet.env.POSTFIX_MATCH .. ");"
     end, {}),
   }),
 
