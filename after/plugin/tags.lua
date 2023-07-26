5d6af613f46e54a3c1d460431555a98cba7c6382
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-07-26 09:27:56 +0100

    Options fix formatting

--- after/plugin/tags.lua
+++ after/plugin/tags.lua
@@ -19,7 +19,7 @@ vim.api.nvim_create_user_command("GutentagsClearCache", 'call system("rm " . g:g
 vim.g.gutentags_generate_on_new = 1
 vim.g.gutentags_generate_on_missing = 1
 vim.g.gutentags_generate_on_write = 1
-vim.g.gutentags_generate_on_empty_buffer = 0
+vim.g.gutentags_generate_on_empty_buffer = 1
 
 vim.g.gutentags_ctags_extra_args = {
   "--tag-relative=yes",
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,37 +1,37 @@
 local options = {
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  ignorecase = true,                       -- ignore case in search
-  smartcase = true,                        -- smart case in search
-  mouse = "a",                             -- enable mouse everywhere
-  pumheight = 10,                          -- pop up menu height, is unlimited by default
-  smartindent = true,                      -- make indenting smarter again
-  autoindent = true,                       -- auto indent
-  splitbelow = true,                       -- split down
-  splitright = true,                       -- split to right
-  undolevels = 10000,                      -- big undo
-  undofile = true,                         -- store undo in a file
-  history = 10000,                         -- big undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  expandtab = true,                        -- tabs -> spaces
-  shiftwidth = 2,                          -- two spaces for each indent
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight current line
-  number = true,                           -- numbered lines
-  relativenumber = true,                   -- relative numbered lines
-  signcolumn = "yes",                      -- always show the sign column. flickers otherwise
-  wrap = false,                            -- show big lines as big lines
-  scrolloff = 8,                           -- always show 8 charcters to bottom
-  sidescrolloff = 8,                       -- always show 8 characters to side of cursor
-  laststatus = 3,                          -- Global statusline
-  lazyredraw = true,                       -- Don't redraw when executing macros
-  magic = true,                            -- Make vim regex more regex-y
-  autoread = true,                         -- re-read files if they are changed
-  termguicolors = true,                    -- use full colour
+  ignorecase = true, -- ignore case in search
+  smartcase = true, -- smart case in search
+  mouse = "a", -- enable mouse everywhere
+  pumheight = 10, -- pop up menu height, is unlimited by default
+  smartindent = true, -- make indenting smarter again
+  autoindent = true, -- auto indent
+  splitbelow = true, -- split down
+  splitright = true, -- split to right
+  undolevels = 10000, -- big undo
+  undofile = true, -- store undo in a file
+  history = 10000, -- big undo
+  updatetime = 300, -- faster completion (4000ms default)
+  expandtab = true, -- tabs -> spaces
+  shiftwidth = 2, -- two spaces for each indent
+  tabstop = 2, -- insert 2 spaces for a tab
+  cursorline = true, -- highlight current line
+  number = true, -- numbered lines
+  relativenumber = true, -- relative numbered lines
+  signcolumn = "yes", -- always show the sign column. flickers otherwise
+  wrap = false, -- show big lines as big lines
+  scrolloff = 8, -- always show 8 charcters to bottom
+  sidescrolloff = 8, -- always show 8 characters to side of cursor
+  laststatus = 3, -- Global statusline
+  lazyredraw = true, -- Don't redraw when executing macros
+  magic = true, -- Make vim regex more regex-y
+  autoread = true, -- re-read files if they are changed
+  termguicolors = true, -- use full colour
 }
 
-vim.opt.shortmess:append("c")         -- don't need extra verbose autocomplete
+vim.opt.shortmess:append("c") -- don't need extra verbose autocomplete
 vim.cmd("set whichwrap+=<,>,[,],h,l") -- movements can wrap over lines
-vim.cmd("set iskeyword+=-")           -- make - part of word
+vim.cmd("set iskeyword+=-") -- make - part of word
 vim.g.mapleader = ","
 
 for k, v in pairs(options) do
@@ -87,8 +87,9 @@ vim.keymap.set("n", "<leader>p", '"+p', opts)
 vim.keymap.set("v", "<leader>p", '"+p', opts)
 vim.keymap.set("v", "<leader>p", '"+p', opts)
 
-vim.keymap.set("n", "<leader>co", '<cmd>copen<cr>', opts)
-vim.keymap.set("n", "<leader>cc", '<cmd>cclose<cr>', opts)
+vim.keymap.set("n", "<leader>co", "<cmd>copen<cr>", opts)
+vim.keymap.set("n", "<leader>cc", "<cmd>cclose<cr>", opts)
 
-vim.keymap.set("n", "<leader>cn", '<cmd>cnext<cr>', opts)
-vim.keymap.set("n", "<leader>cp", '<cmd>cprevious<cr>', opts)
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>", opts)
+vim.keymap.set("n", "<leader>cp", "<cmd>cprevious<cr>", opts)
+
