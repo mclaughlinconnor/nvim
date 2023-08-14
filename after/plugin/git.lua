08f6ee7e3773799f547f79da75bee14456429513
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-08-14 09:12:30 +0100

    Set up neogit as primary git tool, with lazygit as fallback

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -12,7 +12,6 @@ require("gitsigns").setup({
 vim.g.lazygit_floating_window_scaling_factor = 1
 
 local bufopts = { noremap = true, silent = true }
-vim.keymap.set("n", "gh", "<cmd> wall | LazyGit <CR>", bufopts)
 
 require("diffview").setup({
   enhanced_diff_hl = false, -- See |diffview-config-enhanced_diff_hl|
@@ -38,14 +37,11 @@ require("diffview").setup({
 vim.keymap.set("n", "<leader>do", "<cmd> DiffviewOpen <CR>", bufopts)
 vim.keymap.set("n", "<leader>dO", "<cmd> DiffviewClose <CR>", bufopts)
 
--- Weird darker theme, "inline" popups
--- buffer_history_preview
--- buffer_blame_preview
--- require("vgit").setup()
-
--- I just don't like this one
--- the docs are awful which, for such a complex plugin, is unacceptable.
--- Try vimagit
 require("neogit").setup()
 
+vim.keymap.set("n", "gH", "<cmd> wall | LazyGit <CR>", bufopts)
+vim.keymap.set("n", "gh", "<cmd> Neogit <CR>", bufopts)
+
 vim.o.diffopt = "internal,filler,closeoff,icase,iblank,iwhite,algorithm:histogram,indent-heuristic,linematch:50"
+
+
