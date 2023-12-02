70032917a6d794952556b45fae7cc5127fa5091b
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-12-02 12:25:49 +0000

    Bump versions

--- after/plugin/colourscheme.lua
+++ after/plugin/colourscheme.lua
@@ -2,33 +2,22 @@ vim.cmd.colorscheme("nord")
 
 vim.opt.termguicolors = true
 
-require("indent_blankline").setup({
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = false,
-  context_patterns = {
-    "class",
-    "^func",
-    "method",
-    "^if",
-    "while",
-    "for",
-    "with",
-    "try",
-    "except",
-    "arguments",
-    "argument_list",
-    "object",
-    "dictionary",
-    "element",
-    "table",
-    "tuple",
-    "do_block",
-    "tag", -- default with `tag` added
+require("ibl").setup({
+  enabled = true,
+  -- Debug why scope highlighting isn't working properly
+  scope = {
+    enabled = false,
+    include = {
+      node_type = {
+        pug = {
+          "tag",
+          "content",
+          "attributes",
+        },
+      },
+    },
   },
 })
-
-vim.g.indent_blankline_enabled = false
 
 local setColour = function(colour)
   vim.cmd.colorscheme(colour)
--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-require("impatient")
+-- Current nvim HEAD: 17f3a3ae31d91944a5a4e56aa743745cff7fdf07
+
+vim.loader.enable()
 
 vim.filetype.add({
   extension = {
