a93f41e8aae621bcb706ea2c82b04940fbccfe7f
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-12-03 12:04:52 +0000

    Add switch.vim switching for neogit rebase message windows

--- lua/configuration/alternate.lua
+++ lua/configuration/alternate.lua
@@ -2,7 +2,37 @@
 -- My own code could have different keymaps to go to different files instead of just one switch
 
 return {
-  { "AndrewRadev/switch.vim", commit = "68d269301181835788dcdcb6d5bca337fb954395" },
+  {
+    "AndrewRadev/switch.vim",
+    commit = "68d269301181835788dcdcb6d5bca337fb954395",
+    init = function()
+      local switch = vim.api.nvim_create_augroup("SwitchNeogit", {})
+      vim.api.nvim_create_autocmd({ "FileType"}, {
+        callback = function()
+          if not vim.g.loaded_switch then
+            return
+          end
+
+          vim.b.switch_definitions = {
+            { "pick", "fixup", "reword", "edit", "squash", "exec", "break", "drop", "label", "reset", "merge" },
+            { ["^p "] = "fixup " },
+            { ["^f "] = "reword " },
+            { ["^r "] = "edit " },
+            { ["^e "] = "squash " },
+            { ["^s "] = "exec " },
+            { ["^x "] = "break " },
+            { ["^b "] = "drop " },
+            { ["^d "] = "label " },
+            { ["^l "] = "reset " },
+            { ["^t "] = "merge " },
+            { ["^m "] = "pick " },
+          }
+        end,
+        group = switch,
+        pattern = "NeogitRebaseTodo",
+      })
+    end,
+  },
   {
     "ton/vim-alternate",
     commit = "57a6d2797b3bec39f5c075104082b0c0835535ed",
