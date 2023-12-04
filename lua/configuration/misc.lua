29e8e8fca5573b69e7bba7e2a7ba8fb4e5c6d50f
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-12-04 11:06:05 +0000

    [MACOS] Add command for installing pcre with luarocks with homebrew

--- lua/configuration/misc.lua
+++ lua/configuration/misc.lua
@@ -1,6 +1,7 @@
 return {
   { "stevearc/dressing.nvim", commit = "8b7ae53d7f04f33be3439a441db8071c96092d19" },
   { "nvim-lua/plenary.nvim", commit = "55d9fe89e33efd26f532ef20223e5f9430c8b0c0" },
+  -- ~/.local/share/nvim/lazy/luarocks/.rocks/bin/luarocks install lrexlib-pcre2 PCRE2_DIR="/opt/homebrew/opt/pcre2" PCRE2_INCDIR="/opt/homebrew/opt/pcre2/include"
   {
     "camspiers/luarocks",
     opts = {
