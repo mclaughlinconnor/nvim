517a75e06746bb290c5940722a1e8ae702789e10
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-04-28 11:35:19 +0100

    Bump mason

--- lua/configuration/lsp.lua
+++ lua/configuration/lsp.lua
@@ -78,7 +78,7 @@ end
 return {
   {
     "williamboman/mason.nvim",
-    commit = "41e75af1f578e55ba050c863587cffde3556ffa6",
+    commit = "751b1fcbf3d3b783fcf8d48865264a9bcd8f9b10",
     config = function()
       -- Seems to be required
       require("mason").setup()
