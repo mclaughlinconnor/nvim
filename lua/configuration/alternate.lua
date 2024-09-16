58a2a0452b4c597b9e9f6e93d79d3433a2735dde
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-09-16 15:25:07 +0100

    Add dot-repeatable switch on current line

--- lua/configuration/alternate.lua
+++ lua/configuration/alternate.lua
@@ -1,11 +1,10 @@
--- TODO: this extension is completely unnecessary --- just write your own code.
--- My own code could have different keymaps to go to different files instead of just one switch
-
 return {
   {
     "AndrewRadev/switch.vim",
     commit = "68d269301181835788dcdcb6d5bca337fb954395",
     init = function()
+      vim.g.switch_mapping = ""
+
       local switch = vim.api.nvim_create_augroup("SwitchNeogit", {})
       vim.api.nvim_create_autocmd({ "FileType" }, {
         callback = function()
@@ -23,6 +22,54 @@ return {
         group = switch,
         pattern = "typescript",
       })
+
+      vim.cmd([[
+        function! SwitchLine(cnt = v:count1) abort
+          let tick = b:changedtick
+          let start = getpos(".")
+
+          " Call the Switch function cnt times initially
+          for n in range(a:cnt)
+            Switch
+          endfor
+
+          if b:changedtick != tick
+            return
+          endif
+
+          " Loop through the line, switching and moving forward with `w`
+          while v:true
+            let pos = getcurpos()
+            normal! w
+
+            if pos[1] != getcurpos()[1] || pos == getcurpos()
+              break
+            endif
+            for n in range(1)
+              Switch
+            endfor
+            if b:changedtick != tick
+              return
+            endif
+          endwhile
+
+          call setpos('.', start)
+        endfunction
+      ]])
+
+      -- Needs to come after vim-repeat is loaded and this is the only way I could come up with
+      local switchGroup = vim.api.nvim_create_augroup("TypeScriptIndentOnPaste", {})
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        callback = function()
+          vim.cmd([[
+            nnoremap <silent> <Plug>(SwitchInLine) :<C-u>call SwitchLine(v:count1)<CR>:call repeat#set("\<Plug>(SwitchInLine)", v:count1)<CR>
+            nmap gs <Plug>(SwitchInLine)
+          ]])
+        end,
+        group = switchGroup,
+        -- pattern = "*.ts",
+      })
+
       vim.api.nvim_create_autocmd({ "FileType" }, {
         callback = function()
           if not vim.g.loaded_switch then
@@ -49,6 +96,8 @@ return {
       })
     end,
   },
+  -- TODO: this extension is completely unnecessary --- just write your own code.
+  -- My own code could have different keymaps to go to different files instead of just one switch
   {
     "ton/vim-alternate",
     commit = "57a6d2797b3bec39f5c075104082b0c0835535ed",
