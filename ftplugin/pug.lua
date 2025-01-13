827c6beda3d338c80140cda8f92281670e180cdd
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-01-13 10:57:47 +0000

    Pug and TS iskeyword

---
+++ ftplugin/pug.lua
@@ -0,0 +1,1 @@
+vim.cmd("setlocal iskeyword+=-") -- make - part of word
---
+++ ftplugin/typescript.lua
@@ -0,0 +1,2 @@
+vim.cmd("setlocal iskeyword+=+") -- make + part of word
+vim.cmd("setlocal iskeyword+=-") -- make - part of word
