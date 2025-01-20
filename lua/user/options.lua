aaf756d3098e12d16d75635d02b727bf4b7f3c2e
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-01-20 15:55:13 +0000

    Reduce yank highlight time

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -42,7 +42,7 @@ end
 vim.cmd([[
 augroup highlight_yank
 autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=50})
 augroup END
 ]])
 
