48ba4d8cbc2dfc8413189a067093a486a8af1f21
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-01-21 11:11:32 +0000

    Add typescript 120 gutter column

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,2 +1,3 @@
 vim.cmd("setlocal iskeyword+=+") -- make + part of word
 vim.cmd("setlocal iskeyword+=-") -- make - part of word
+vim.cmd("set colorcolumn=121") -- The column is to the wall that may not be touched or crossed
