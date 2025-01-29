a5ed11b14945527092ad743cf594979782fb414b
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-01-29 14:37:45 +0000

    Fix indent on paste typescript

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,3 +1,11 @@
 vim.cmd("setlocal iskeyword+=+") -- make + part of word
 vim.cmd("setlocal iskeyword+=-") -- make - part of word
-vim.cmd("set colorcolumn=121") -- The column is to the wall that may not be touched or crossed
+vim.cmd("set colorcolumn=121") -- The column is the wall that may not be touched or crossed
+
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_buf_set_keymap(0, "n", "p", "p`[v`]=", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "P", "P`[v`]=", opts)
+
+vim.api.nvim_buf_set_keymap(0, "v", "p", "p`[v`]=", opts)
+vim.api.nvim_buf_set_keymap(0, "v", "P", "P`[v`]=", opts)
