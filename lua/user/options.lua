c9b6edda22bdc9a815fdae5a269e8efdff00c2ef
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-03-14 12:05:09 +0000

    Add <leader>/ visual keybind to start search with selection

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -86,6 +86,8 @@ vim.keymap.set("n", "<leader>cc", "<cmd>cclose<cr>", opts)
 vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>", opts)
 vim.keymap.set("n", "<leader>cp", "<cmd>cprevious<cr>", opts)
 
+vim.keymap.set('x', '<leader>/', '<Esc>/\\%V')
+
 local pwd = vim.api.nvim_create_augroup("PWD", {})
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
   callback = function()
