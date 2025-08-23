4b7a95d79b15cae64f3c18c6014761f3023772d7
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-08-23 17:51:01 +0100

    Remove annoying textwidth config

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,5 +1,3 @@
-vim.bo.textwidth = 120
-
 vim.cmd("setlocal iskeyword+=+") -- make + part of word
 vim.cmd("setlocal iskeyword+=-") -- make - part of word
 vim.cmd("set colorcolumn=121") -- The column is the wall that may not be touched or crossed
