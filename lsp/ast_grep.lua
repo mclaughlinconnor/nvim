da195e9194c69a36d9a2dd640908274ef93ce53f
Connor McLaughlin <connor1211.mcl@gmail.com>
2026-01-07 17:27:52 +0000

    Fix ast_grep not starting

--- lsp/ast_grep.lua
+++ lsp/ast_grep.lua
@@ -1,4 +1,6 @@
 return {
   cmd = { "sg", "lsp", "--config", vim.fn.fnamemodify(vim.fn.expand("$MYVIMRC"), ":h") .. "/misc/ast-grep/sgconfig.yml" },
+  root_dir = vim.fn.getcwd(),
   filetypes = { "typescript", "pug" },
+  name = "ast_grep"
 }
