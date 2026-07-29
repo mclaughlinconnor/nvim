6da228b5f21e61e913a4edad6aa71d03e300f128
Connor McLaughlin <connor1211.mcl@gmail.com>
2026-07-29 15:30:33 +0100

    Remove now unused luarocks and dressing.nvim

--- lua/configuration/misc.lua
+++ lua/configuration/misc.lua
@@ -4,26 +4,7 @@ return {
     commit = "8c1b4ec6a48d038a79ced8674cb15e7db6dd8ef0",
     opts = {},
   },
-  {
-    "stevearc/dressing.nvim",
-    commit = "2d7c2db2507fa3c4956142ee607431ddb2828639",
-    opts = {
-      input = {
-        insert_only = false,
-      },
-    },
-  },
   { "nvim-lua/plenary.nvim", commit = "857c5ac632080dba10aae49dba902ce3abf91b35" },
-  -- ~/.local/share/nvim/lazy/luarocks/.rocks/bin/luarocks install lrexlib-pcre2 PCRE2_DIR="/opt/homebrew/opt/pcre2" PCRE2_INCDIR="/opt/homebrew/opt/pcre2/include"
-  {
-    "camspiers/luarocks",
-    opts = {
-      rocks = {
-        "luautf8",
-        "lrexlib-pcre2",
-      },
-    },
-  },
   { "tpope/vim-abolish", commit = "dcbfe065297d31823561ba787f51056c147aa682" },
   {"tpope/vim-repeat", commit = "65846025c15494983dafe5e3b46c8f88ab2e9635"},
 }
